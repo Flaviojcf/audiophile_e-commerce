d9838951d60c4d8becff73ff1e32bc4511df4f31
Flavio <fjcf@poli.br>
2022-10-30 14:54:21 -0300

    feat: add default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/default";
+
 function App() {
-  return <div>App</div>;
+  return <ThemeProvider theme={defaultTheme}></ThemeProvider>;
 }
 
 export default App;
