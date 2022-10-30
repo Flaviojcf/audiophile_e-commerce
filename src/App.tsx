3a3e6936513332d00f7750fccdb0694caecc01f6
Flavio <fjcf@poli.br>
2022-10-30 15:27:55 -0300

    feat: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/theme/default";
 
 function App() {
-  return <ThemeProvider theme={defaultTheme}></ThemeProvider>;
+  return (
+  <ThemeProvider theme={defaultTheme}>
+    <GlobalStyle />
+    
+  </ThemeProvider>
+  )
 }
 
 export default App;
