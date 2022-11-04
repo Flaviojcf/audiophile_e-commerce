76bd0590e6011384b53205c330ddf2eef0d1dd6b
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-11-04 09:10:54 -0300

    lint: fix files with eslint

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,59 +1,59 @@
-import { CopyrightText, FooterContainer, FooterContainerHeader, Rectangle, SocialsContainer } from "./styles";
+import { CopyrightText, FooterContainer, FooterContainerHeader, Rectangle, SocialsContainer } from './styles'
 
 import audioPhileImg from '../../assets/audiophile.svg'
-import { FacebookLogo, InstagramLogo, TwitterLogo } from "phosphor-react";
+import { FacebookLogo, InstagramLogo, TwitterLogo } from 'phosphor-react'
 
 export function Footer () {
-    return (
-        <FooterContainer>
-            <Rectangle />
+  return (
+    <FooterContainer>
+      <Rectangle />
 
-            <FooterContainerHeader>
-                <img src={audioPhileImg} alt="Audiophile logo" />
-                <ul>
-                    <li>
-                        <a href="#">HOME</a>
-                    </li>
-                    <li>
-                        <a href="#">HEADPHONES</a>
-                    </li>
-                    <li>
-                        <a href="#">SPEAKERS</a>
-                    </li>
-                    <li>
-                        <a href="#">EARPHONES</a>
-                    </li>
-                </ul>
-            </FooterContainerHeader>
+      <FooterContainerHeader>
+        <img src={audioPhileImg} alt="Audiophile logo" />
+        <ul>
+          <li>
+            <a href="#">HOME</a>
+          </li>
+          <li>
+            <a href="#">HEADPHONES</a>
+          </li>
+          <li>
+            <a href="#">SPEAKERS</a>
+          </li>
+          <li>
+            <a href="#">EARPHONES</a>
+          </li>
+        </ul>
+      </FooterContainerHeader>
 
-            <p>
-                Audiophile is an all in one stop to fulfill your audio needs.
-                We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio.
-                Come and visit our demo facility - we’re open 7 days a week.
-            </p>
+      <p>
+        Audiophile is an all in one stop to fulfill your audio needs.
+        We&apos;re a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio.
+        Come and visit our demo facility - we’re open 7 days a week.
+      </p>
 
-            <CopyrightText>
-                Copyright 2022. All Rights Reserved
-            </CopyrightText>
+      <CopyrightText>
+        Copyright 2022. All Rights Reserved
+      </CopyrightText>
 
-            <SocialsContainer>
-                <li>
-                    <a href="#">
-                        <FacebookLogo size={24} weight='fill' />
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <TwitterLogo size={24} weight='fill' />
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <InstagramLogo size={24} />
-                    </a>
-                </li>
-            </SocialsContainer>
+      <SocialsContainer>
+        <li>
+          <a href="#">
+            <FacebookLogo size={24} weight='fill' />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <TwitterLogo size={24} weight='fill' />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <InstagramLogo size={24} />
+          </a>
+        </li>
+      </SocialsContainer>
 
-        </FooterContainer>
-    )
+    </FooterContainer>
+  )
 }
