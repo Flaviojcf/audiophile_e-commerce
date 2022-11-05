937edf2b48c7b026f8cba8c40d44039ef0bb9c5a
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-11-05 12:20:31 -0300

    feat: add Home page without content

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Footer } from '../../components/Footer'
+
+import { HomeContainer, HomeContent } from './styles'
+
+export function Home () {
+  return (
+    <HomeContainer>
+      <HomeContent>
+      </HomeContent>
+      <Footer />
+    </HomeContainer>
+  )
+}
