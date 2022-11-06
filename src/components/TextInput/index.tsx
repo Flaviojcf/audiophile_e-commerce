f691063975fd73fed31e1ccc99faeda5c55ba760
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-11-06 15:34:47 -0300

    feat: pass error message to Input component

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -3,17 +3,18 @@ import { InputContainer } from './styles'
 
 type TextInputProps = InputHTMLAttributes<HTMLInputElement> & {
   hasError: boolean
+  errorMessage?: string
   label: string
   placeholder: string
   isLargeSize: boolean
 }
 
-export function TextInput ({ hasError, label, placeholder, isLargeSize, ...rest }: TextInputProps) {
+export function TextInput ({ hasError, label, placeholder, isLargeSize, errorMessage = 'Wrong format', ...rest }: TextInputProps) {
   return (
     <InputContainer hasError={hasError} isLargeSize={isLargeSize}>
       <div>
         <label>{label}</label>
-        <span>Wrong format</span>
+        <span>{errorMessage}</span>
       </div>
       <input
         type="text"
