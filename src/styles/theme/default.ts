aa17e71aecca0b64c84390e6af74579e780085c3
Flavio <fjcf@poli.br>
2022-11-10 18:17:02 -0300

    feat: add new color to default theme

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -4,12 +4,13 @@ export const defaultTheme = {
     'orange-100': '#fbaf85',
 
     'black-100': '#101010',
-
+    'black-200': '#191919',
     black: '#000000',
 
     'gray-300': '#4C4C4C',
     'gray-200': '#F1F1F1',
     'gray-100': '#FAFAFA',
+    'gray-50': '#CFCFCF',
 
     white: '#FFFFFF',
     'white-100': '#f2f2f2'
