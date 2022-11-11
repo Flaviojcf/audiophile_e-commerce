bdb3c7d1a08bb70ff2b2dc253eb911500741b308
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-11-11 12:02:18 -0300

    feat: use ProductInfo component in ProductDetails page

--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -1,10 +1,40 @@
 // import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 import { ProductCategoriesNavigator } from '../../components/ProductCategoriesNavigator'
 import { GoBackButton, ProductDetailsContainer, ProductDetailsContent } from './styles'
 
+import productsJSON from '../../data/products.json'
+import { ProductType } from '../Products'
+import { ProductInfo } from './components/ProductInfo'
+
 export function ProductDetails () {
+  const [product, setProduct] = useState<ProductType>(null as any)
+
   const navigate = useNavigate()
+  const { productSlug, category } = useParams()
+
+  useEffect(() => {
+    const productBySlugAndCategory = productsJSON.find(product => product.slug === productSlug && product.category === category)
+
+    if (!productBySlugAndCategory) {
+      navigate('/')
+    }
+
+    const formattedProduct: ProductType = {
+      id: productBySlugAndCategory!.id,
+      name: productBySlugAndCategory!.name,
+      description: productBySlugAndCategory!.description,
+      imageUrl: productBySlugAndCategory!.image.desktop,
+      isNew: productBySlugAndCategory!.new,
+      price: productBySlugAndCategory!.price,
+      features: productBySlugAndCategory!.features,
+      slug: productBySlugAndCategory!.slug,
+      includes: productBySlugAndCategory!.includes
+    }
+
+    setProduct(formattedProduct)
+  }, [])
 
   return (
     <ProductDetailsContainer className='container'>
@@ -15,7 +45,7 @@ export function ProductDetails () {
       </GoBackButton>
 
       <ProductDetailsContent>
-
+        {product && <ProductInfo product={product} />}
       </ProductDetailsContent>
 
       <ProductCategoriesNavigator />
