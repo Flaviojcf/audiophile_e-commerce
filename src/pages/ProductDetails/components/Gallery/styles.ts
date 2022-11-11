fe185070be44de3093fea16cceecdda4617107a1
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-11-11 17:27:40 -0300

    feat: make Gallery component responsive for medium size screen devices

--- src/pages/ProductDetails/components/Gallery/styles.ts
+++ src/pages/ProductDetails/components/Gallery/styles.ts
@@ -3,12 +3,23 @@ import styled from 'styled-components'
 export const GalleryContainer = styled.div`
   display: flex;
   gap: 1.875rem;
+
+  margin: 0 auto;
+
+  @media (max-width: 1080px) and (min-width: 768px) {
+    gap: 1.125rem;
+  }
+
 `
 
 export const SmallImagesContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 2rem;
+
+  @media (max-width: 1080px) and (min-width: 768px) {
+    gap: 1.25rem;
+  }
 `
 
 export const SmallImageContainer = styled.div`
@@ -22,6 +33,11 @@ export const SmallImageContainer = styled.div`
     height: 100%;
     border-radius: 8px;
   }
+
+  @media (max-width: 1080px) and (min-width: 768px) {
+    width: 17.3125rem;
+    height: 10.875rem;
+  }
 `
 
 export const ImageContainer = styled.div`
@@ -34,5 +50,10 @@ export const ImageContainer = styled.div`
     width: 100%;
     height: 100%;
     border-radius: 8px;
+  }
+
+  @media (max-width: 1080px) and (min-width: 768px) {
+    width: 24.6875rem;
+    height: 23rem;
   }
 `
